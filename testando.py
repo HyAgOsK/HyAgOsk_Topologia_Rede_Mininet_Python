f7282b2a6e2c779c16d0bbd767fcda9c360f8688
hyago <hyagovieira@geb.inatel.br>
2021-11-19 18:53:35 -0300

    arquivo python para topologia

---
+++ testando.py
@@ -0,0 +1,33 @@
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "3 host 2 switch 2 host custom topology"
+
+    def init( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.init( self )
+
+        # Add hosts and switches
+        leftHost = self.addHost( 'h1' )
+        leftHost1a=self.addHost('h1a')
+        centerHost=self.addHost('h2')
+        centerHost2a=self.addHost('h2a')
+        rightHost = self.addHost( 'h3' )
+        rightHost3a = self.addHost( 'h3a' )
+        leftSwitch = self.addSwitch( 's1' )
+        centerSwitch = self.addSwitch( 's2' )
+        rightSwitch = self.addSwitch( 's3' )
+
+        # Add links
+        self.addLink( leftHost, leftSwitch )
+        self.addLink( leftHost1a, leftSwitch )
+        self.addLink( centerHost, centerSwitch )
+        self.addLink( centerHost2a, centerSwitch )
+        self.addLink( rightHost, rightSwitch )
+        self.addLink( rightHost3a, rightSwitch )
+        self.addLink( leftSwitch, centerSwitch,  )
+        self.addLink( centerSwitch, rightSwitch )
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
