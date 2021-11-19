f29152ece647f42484c4873cd8e6bc6bffdebbe3
HyAgOsK <70180407+HyAgOsK@users.noreply.github.com>
2021-11-19 19:25:23 -0300

    udpate code

--- testando.py
+++ testando.py
@@ -3,31 +3,31 @@ from mininet.topo import Topo
 class MyTopo( Topo ):
     "2 host 3 switch 4 host custom topology"
 
-    def init( self ):
+    def __init__( self ):
         "Create custom topo."
 
         # Initialize topology
-        Topo.init( self )
+        Topo.__init__( self )
 
         # Add hosts and switches
         leftHost = self.addHost( 'h1' )
-        leftHost2 = self.addHost('h2')
-        centerHost3 = self.addHost('h3')
-        centerHost4 = self.addHost('h4')
-        rightHost5 = self.addHost( 'h5' )
-        rightHost6 = self.addHost( 'h6' )
+        leftHost1a = self.addHost('h1a')
+        centerHost2 = self.addHost('h2')
+        centerHost2a = self.addHost('h2a')
+        rightHost = self.addHost( 'h3' )
+        rightHost3a = self.addHost( 'h3a' )
         leftSwitch = self.addSwitch( 's1' )
         centerSwitch = self.addSwitch( 's2' )
         rightSwitch = self.addSwitch( 's3' )
 
         # Add links
         self.addLink( leftHost, leftSwitch )
-        self.addLink( leftHost2, leftSwitch )
+        self.addLink( leftHost1a, leftSwitch )
         self.addLink( leftSwitch, centerSwitch )
-        self.addLink( centerHost3, centerSwitch )
-        self.addLink( centerHost4, centerSwitch )
+        self.addLink( centerHost2, centerSwitch )
+        self.addLink( centerHost2a, centerSwitch )
         self.addLink( centerSwitch, rightSwitch )
-        self.addLink( rightHost5, rightSwitch )
-        self.addLink( rightHost6, rightSwitch )
+        self.addLink( rightHost, rightSwitch )
+        self.addLink( rightHost3a, rightSwitch )
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
